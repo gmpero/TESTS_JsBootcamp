32da835b8428d70b191eeb3a137df792f844624a
Rostislav <penshinrost@gmail.com>
2025-11-22 11:49:34 +0300

    add US_02.001

--- tests/helpers/pero.js
+++
@@ -1,5 +0,0 @@
-export const ItemsOnDropdown = {
-    name : "test",
-    expectedCount : 7,
-    listItemMenu : ["Changes", "Workspace", "Build Now", "Configure", "Delete Project", "Rename", "Credentials"],
-}
---
+++ tests/testData/US_02.001.testData.js
@@ -0,0 +1,20 @@
+// US_02.001.testData.js
+export const ProjectData = {
+    freestyleProject: {
+        name: "test-freestyle",
+        description: "Test freestyle project for US_02.001"
+    }
+};
+
+export const DropdownData = {
+    expectedItemCount: 7,
+    menuItems: [
+        "Changes", 
+        "Workspace", 
+        "Build Now", 
+        "Configure", 
+        "Delete Project", 
+        "Rename", 
+        "Credentials"
+    ]
+};
